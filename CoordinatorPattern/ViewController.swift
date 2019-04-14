80c0293ba89cedba41bed9c9ec038af1dc910b55
Ayman Ibrahim <ayman.ibrahim.alim@gmail.com>
2019-04-14 13:50:36 +0300

    Initial Commit

---
+++ CoordinatorPattern/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoordinatorPattern
+//
+//  Created by Ayman Ibrahim on 4/14/19.
+//  Copyright © 2019 Ayman Ibrahim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
